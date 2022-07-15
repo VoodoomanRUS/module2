c10c665391f96fc5bae95b45afb09564cbd18ef2
VoodoomanRUS <voodooman88@gmail.com>
2022-07-15 16:43:44 +0700

    Changes 2

--- src/main/java/org/example/lesson2/Homework1.java
+++ src/main/java/org/example/lesson2/Homework1.java
@@ -1,7 +1,18 @@
 package org.example.lesson2;
 
+import org.example.homework1.NumberList;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Homework1 {
     public static void main(String[] args) {
+        NumberList<Number> myNewList = new NumberList<>(Number.class);
+
+        for (int i = 0; i < 10; i++) {
+            myNewList.add(i, 5);
+        }
+        System.out.println(Arrays.toString(myNewList.toArray()));
         //Создать свою реализацию List - (Number list). Она может работать только с Numbers (Generics)
         //Особенности переопределения методов
         //1. Метод add - который добавляет в начало массива
