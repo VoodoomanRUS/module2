c8b2db6196cb0e4ca32c620bd4aee85fe8907030
Dmitrii But <voodooman88@gmail.com>
2022-07-17 18:45:01 +0700

    Homework1 done

--- src/main/java/org/example/lesson2/Lesson2.java
+++ src/main/java/org/example/lesson2/Lesson2.java
@@ -1,11 +1,13 @@
 package org.example.lesson2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class Lesson2 {
     public static void main(String[] args) {
         MyList<String> list = new MyList<>(String.class);
+        System.out.println(Arrays.toString(list.toArray()));
         list.add("hello");
         list.add("hello2");
         list.add("hello3");
