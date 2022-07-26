2f0b2e2781ef54ec341f5a598c9ed5f1c647dda5
Dmitrii But <voodooman88@gmail.com>
2022-07-26 14:59:39 +0700

    Homework2 done

--- src/main/java/org/example/lesson4/Task2.java
+++ src/main/java/org/example/lesson4/Task2.java
@@ -1,5 +1,7 @@
 package org.example.lesson4;
 
+import org.example.homework2.MyUser;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
@@ -9,8 +11,18 @@ import java.util.stream.Stream;
 public class Task2 {
     public static void main(String[] args) {
         //Дан лист
-        List<Integer> integers = List.of(1,2,3,4,5,5,8,8,9);
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 5, 8, 8, 9);
         Random random = new Random(1);
+        integers.stream()
+                .filter(e -> e > 4)
+                .distinct()
+                .map(e -> new MyUser(e, Stream.generate(() -> random.nextInt(10)).limit(e)
+                        .collect(Collectors.toList())))
+                .map(MyUser::getUserList)
+                .flatMap(Collection::stream)
+                .map(e -> e * 10)
+                .reduce(Integer::sum)
+                .ifPresentOrElse(System.out::println, () -> System.out.println(0));
 
         //Задача №1
         //Создать класс пользователей, каждый из которых будет иметь порядковый номер, и список с числами
@@ -33,14 +45,25 @@ public class Task2 {
         // 1120 - из примера, 0 - при пустом листе
 
         List<List<Integer>> lists = List.of(List.of(1, 2), List.of(3, 4, 5), List.of());
+
+
         //Задача №2
         // 1. Отсортировать список lists так, чтобы сначала были самые большие списки по размеру
         // 2. Вывести на экран все элементы
         // Ожидаемый результат: 3,4,5,1,2
-        
+
+        lists.stream()
+                .sorted((e, e1) -> e1.size() - e.size())
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
         //Задача №3
         // 1. Узнать, есть ли в lists хотя бы один список, который содержит сумму всех элементов вложенного листа
         // равную 12
+
+        System.out.println(lists.stream().anyMatch(e -> e.stream().reduce(Integer::sum).orElse(0).equals(12)));
+
 
     }
 }
